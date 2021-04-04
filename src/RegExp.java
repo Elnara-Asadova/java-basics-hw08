473679bea9ba252a87e043ce706e5e465aeaffad
Elnara Asadova <asadova.eln@gmail.com>
2021-04-04 15:56:52 +0300

    IP address regExp

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,16 +1,28 @@
-/**
- * Regular expressions.
- */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegExp {
 
-    /**
-     * Get IP address from text.
-     *
-     * @param text text to parse
-     * @return String with IP address
-     */
+    public static final String IPV4_PATTERN =
+            "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\." +
+                    "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\." +
+                    "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\." +
+                    "([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])";
+
+    public static final String EXTRACT_PATTERN = "\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\b";
+
+    private static final Pattern extract_pattern = Pattern.compile(EXTRACT_PATTERN);
+    private static final Pattern valid_pattern = Pattern.compile(IPV4_PATTERN);
+
     public static String getIpAddress(String text) {
-        // TODO fill in code here for extracting IP address using Regular Expression
-        return "";
+            Matcher matcher = extract_pattern.matcher(text);
+            if (matcher.find()) {
+                Matcher valid_matcher = valid_pattern.matcher(matcher.group());
+                if (valid_matcher.matches())
+                    return valid_matcher.group();
+            }
+
+            return "";
     }
 }
